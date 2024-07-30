6aa4f91209ea407e9d09cff36655d6dd694892e2
Elina Pakharevskaya <elina.paxar@gmail.com>
2024-07-30 14:12:49 +0300

    add new data model (#12)

--- internal/entity/user.go
+++ internal/entity/user.go
@@ -1,5 +1,9 @@
 package entity
 
+type UIDRequest struct {
+	UID uint32 `json:"user_id" validate:"number,required"`
+}
+
 type User struct {
 	PasspotNumber string `validate:"number,required"`
 	Surname       string `validate:"alpha,required" json:"surname"`
@@ -7,7 +11,3 @@ type User struct {
 	Patronymic    string `validate:"alpha,required" json:"patronymic"`
 	Adress        string `validate:"required" json:"adress"`
 }
-
-var (
-	UID string
-)
